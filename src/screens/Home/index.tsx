1fcdd7d7f45b1041d07db23934271eb1ee99a3b5
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2022-10-19 22:43:29 -0300

    Added input form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { Image, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { PlusCircle } from "phosphor-react-native";
+// import { PlusCircle } from "phosphor-react-native";
 
 import Logo from "../../assets/Logo.png";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [participantName, setParticipantName] = useState("");
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logo} />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nombre de participante'
+          placeholderTextColor='#808080'
+          onChangeText={setParticipantName}
+          value={participantName}
+        />
+        <TouchableOpacity style={styles.button}>
+          {/* <Text style={styles.buttonText}> */}
+          <PlusCircle size={24} color='white' />
+          {/* </Text> */}
+        </TouchableOpacity>
+      </View>
+
       <View style={styles.task}>
         <View>
           <Text style={styles.infoTitle}>Criadas</Text>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,16 +3,20 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0D0D0D'
+    backgroundColor:'#0D0D0D'
+   
   },
   logo: {
     alignSelf:'center',
     width: 110,
     height: 32,
     marginTop: 70,
-    marginBottom: 30
+     marginBottom: 71
   },
   task:{
+    flex: 1,
+   paddingTop: 55,
+    backgroundColor:  '#454545',
     paddingHorizontal: 24,
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -28,5 +32,39 @@ export const styles = StyleSheet.create({
     fontFamily: 'Inter_700Bold',
     lineHeight: 17,
     color: '#8284FA'
-  }
+  },
+  form: {
+    position: 'absolute',
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: '40%',
+    paddingHorizontal: 24,
+    zIndex: 1
+    
+  },
+  input: {
+    flex:1,
+    height: 54,
+    borderRadius: 5,
+    color: '#F2F2F2',
+    fontFamily: 'Inter_400Regular',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 4,
+    backgroundColor: '#262626',
+    zIndex: 1,
+  },
+  button :{
+    width: 52,
+    height: 52,
+    borderRadius: 6,
+    backgroundColor: '#1E6F9F',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24
+  },
 });
